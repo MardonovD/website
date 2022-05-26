e14e13af9615d682cb011ca7f25cd602e0aedcd5
Dostonbek <mardonovdoston69@gmail.com>
2022-05-26 18:28:24 +0300

    navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header__content">
-        <h1 className="header__content-h1">About Us</h1>
+        <h1 className="header__content-h1">Xush Kelibsiz</h1>
         <div className="header__content-links">
           <a href="/home">Home</a>
           <span className="header__content-span"></span>
