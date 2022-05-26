d30e9465aca6dd3649627827e36368f597912037
Dostonbek <mardonovdoston69@gmail.com>
2022-05-26 17:03:48 +0300

    mixins

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
 import "./sass/main.scss";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
