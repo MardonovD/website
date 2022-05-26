32ef441d8e6250e3ad7aec1d3e09ce60e4cdf823
Dostonbek <mardonovdoston69@gmail.com>
2022-05-26 17:24:38 +0300

    main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,22 @@
-import React from 'react';
-
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='header'>
-      
-      
+    <div className="header">
+      <div className="header__content">
+        <h1 className="header__content-h1">About Us</h1>
+        <div className="header__content-links">
+          <a href="/home" className="header__content-link">
+            Home
+          </a>
+          <span className="header__content-span">/</span>
+          <a href="/about" className="header__content-link">
+            About Us
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
-
-
 
 export default Header;
