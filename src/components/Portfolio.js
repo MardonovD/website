b5c86a9ad001426e17d79e2f250248a9de88b4f0
Dostonbek <mardonovdoston69@gmail.com>
2022-05-27 14:39:28 +0300

    our time

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,14 +1,22 @@
-import React from 'react';
-
+import React from "react";
 
 const Portfolio = () => {
   return (
-    <div className='portfolio'>
-      
+    <div className="portfolio">
+      <div className="container">
+        <div className="portfolio__section">
+          <div className="row">
+            <div className="col-4">
+              <div className="portfolio__content">
+                <h6 className="portfolio__content-h6">EMPLOYEES</h6>
+                <h3 className="portfolio__content-h3">OUR TIME.</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
-
-
 
 export default Portfolio;
