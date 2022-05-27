db51894103e9fa676b4fb9371172f64f097281de
Dostonbek <mardonovdoston69@gmail.com>
2022-05-27 13:43:31 +0300

    lorems

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -10,6 +10,13 @@ const Showcase = () => {
         </div>
         <div className="show__card">
           <h6 className="show__card-h6">OUR HAPPY CLIENTS</h6>
+          <div className="show__card-h4">What Client's Say?</div>
+          <div className="show__card-text">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+            illum sed, pariatur porro temporibus deleniti voluptatem enim sit
+            soluta repellendus ea nulla et voluptate adipisci fugit minus
+            facilis? Delectus, magnam.
+          </div>
         </div>
       </div>
     </div>
