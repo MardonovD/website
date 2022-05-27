43629d3f63293ef9aa9f397a3c60d350f9bbf1ce
Dostonbek <mardonovdoston69@gmail.com>
2022-05-27 07:04:01 +0300

    about page

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <h4 className="about-h4">Who we Are</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
