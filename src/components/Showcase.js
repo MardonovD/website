f8f44937fdb8ddf941968a8927bc54ff69ba0202
Dostonbek <mardonovdoston69@gmail.com>
2022-05-27 13:36:53 +0300

    card

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,9 +3,13 @@ import React from "react";
 const Showcase = () => {
   return (
     <div className="show">
-      <div className="container">
+      <div className="container rel">
         <div className="show__content">
-          <h1>Sothat'sus.There snootherwaytopupit.</h1>
+          <h1>Sothat'sus.There </h1>
+          <h1>snootherwaytopupit. </h1>
+        </div>
+        <div className="show__card">
+          <h6 className="show__card-h6">OUR HAPPY CLIENTS</h6>
         </div>
       </div>
     </div>
