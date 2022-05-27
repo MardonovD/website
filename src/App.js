1cac131da78e554710be9b3823add43a1fc4b4f6
Dostonbek <mardonovdoston69@gmail.com>
2022-05-27 07:16:07 +0300

    grid layouts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import "./sass/main.scss";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Header />
       <Navbar />
+      <About />
     </div>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,22 @@ const About = () => {
       <div className="container">
         <div className="row">
           <div className="col-3">
-            <h4 className="about-h4">Who we Are</h4>
+            <h4 className="about-h4">Who we Are ?</h4>
+          </div>
+          <div className="col-9">
+            <div className="about-text">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit autem illo dignissimos debitis beatae quasi,
+                adipisci quam quis voluptas hic provident ratione explicabo sed
+                vitae et ea ex, mollitia officiis error qui? Maiores ab alias,
+                in et enim quo cum.Lorem ipsum, dolor sit amet consectetur
+                adipisicing elit. Quod iusto fugiat, sed ex non reiciendis
+                aspernatur aut libero, officia quia esse praesentium iure ipsum
+                incidunt officiis saepe laborum fuga ratione recusandae alias
+                tempora, laudantium enim quas? Quasi eaque aperiam molestias.
+              </p>
+            </div>
           </div>
         </div>
       </div>
