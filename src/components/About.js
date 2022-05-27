31b12a1f6ac7542b3b6c0f54fed86b93c7321eb0
Dostonbek <mardonovdoston69@gmail.com>
2022-05-27 09:26:14 +0300

    maps

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,29 @@ import React, { useState } from "react";
 
 const About = () => {
   const [state] = useState({ heading: "Who we Are ?" });
+  const [sections] = useState([
+    {
+      heading: "Digital Marketing",
+      text: "Tempore corrupti temporibus fuga earum asperiores fugit",
+      id: 1,
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="2.5rem"
+          height="2.5rem"
+          fill="currentColor"
+          class="bi bi-bag-dash"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.5 10a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"
+          />
+          <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+        </svg>
+      ),
+    },
+  ]);
   return (
     <div className="about">
       <div className="container">
@@ -25,36 +48,22 @@ const About = () => {
             </div>
           </div>
         </div>
-        <div className="row pl-15 mt-60">
-          <div className="col-4">
-            <div className="about__section">
-              <div className="about__section-icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="2.5rem"
-                  height="2.5rem"
-                  fill="currentColor"
-                  class="bi bi-bag-dash"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.5 10a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"
-                  />
-                  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                </svg>
+        <div className="row  mt-60">
+          {sections.map((section) => {
+            return (
+              <div className="col-4 pl-15" key={section.id}>
+                <div className="about__section">
+                  <div className="about__section-icon">{section.icon}</div>
+                  <div className="about__section-heading">
+                    <h6>{section.heading}</h6>
+                  </div>
+                  <div className="about__section-text ">
+                    <p>{section.text}</p>
+                  </div>
+                </div>
               </div>
-              <div className="about__section-heading">
-                <h6>Digital Marketing</h6>
-              </div>
-              <div className="about__section-text ">
-                <p>
-                  Tempore corrupti temporibus fuga earum asperiores fugit
-                  laudantium.
-                </p>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
