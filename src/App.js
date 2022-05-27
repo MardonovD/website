a356ffb708e4f809345cb96dfda19534a80df388
Dostonbek <mardonovdoston69@gmail.com>
2022-05-27 13:23:15 +0300

    image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Showcase from "./components/Showcase";
 import "./sass/main.scss";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Navbar />
       <About />
+      <Showcase />
     </div>
   );
 }
