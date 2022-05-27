6c80c04b8b98f8740b22281e132e35c15bf4555f
Dostonbek <mardonovdoston69@gmail.com>
2022-05-27 16:10:34 +0300

    row

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,7 +13,31 @@ const Portfolio = () => {
               </div>
             </div>
             <div className="col-8">
-              <div className="row">
+              <div className="row ">
+                <div className="col-4  pl-15">
+                  <div className="portfolio-card">
+                    <div className="portfolio-card-image">
+                      <img
+                        src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=240&h=750&dpr=1"
+                        alt="card"
+                      />
+                    </div>
+                    <h5 className="portfolio__card-name"> Mardonov Doston</h5>
+                    <p className="portfolio__card-expert">Full-Stack</p>
+                  </div>
+                </div>
+                <div className="col-4  pl-15">
+                  <div className="portfolio-card">
+                    <div className="portfolio-card-image">
+                      <img
+                        src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=240&h=750&dpr=1"
+                        alt="card"
+                      />
+                    </div>
+                    <h5 className="portfolio__card-name"> Mardonov Doston</h5>
+                    <p className="portfolio__card-expert">Full-Stack</p>
+                  </div>
+                </div>
                 <div className="col-4  pl-15">
                   <div className="portfolio-card">
                     <div className="portfolio-card-image">
