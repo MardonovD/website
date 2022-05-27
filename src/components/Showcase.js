a105d1ce4a00ca337e0074e33c2913a64116430c
Dostonbek <mardonovdoston69@gmail.com>
2022-05-27 13:29:32 +0300

    sothat'sus

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Showcase = () => {
-  return <div className="show">
-    
-  </div>;
+  return (
+    <div className="show">
+      <div className="container">
+        <div className="show__content">
+          <h1>Sothat'sus.There snootherwaytopupit.</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Showcase;
